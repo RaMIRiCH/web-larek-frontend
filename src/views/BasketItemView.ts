7bd864e3bd7b7456ed92ecafa8a344be6f527318
Artem Marin <RaMIRiCH09@yandex.ru>
2025-07-01 10:22:42 +0300

    verso 4

--- src/views/BasketItemView.ts
+++ src/views/BasketItemView.ts
@@ -12,18 +12,14 @@ export class BasketItemView {
     private template: HTMLTemplateElement,
     private eventEmitter: EventEmitter
   ) {
-    // Клонируем шаблон и сохраняем корневой контейнер
     this.container = template.content.firstElementChild!.cloneNode(true) as HTMLElement;
 
-    // Находим DOM-узлы и сохраняем их
     this.indexElem = this.container.querySelector('.basket__item-index')!;
     this.titleElem = this.container.querySelector('.card__title')!;
     this.priceElem = this.container.querySelector('.card__price')!;
     this.removeButton = this.container.querySelector('.basket__item-delete')!;
 
-    // Подписываемся на клик удаления
     this.removeButton.addEventListener('click', () => {
-      // В emit передаем id продукта, который будет передан при вызове updateContent
       if (this.currentProduct) {
         this.eventEmitter.emit('basket:remove', { productId: this.currentProduct.id });
       }
@@ -32,7 +28,6 @@ export class BasketItemView {
 
   private currentProduct: Product | null = null;
 
-  // Метод обновляет содержимое в DOM
   public updateContent(product: Product, index: number): void {
     this.currentProduct = product;
 
